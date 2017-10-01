116ceed0c33d87909b0003310622a94080383911
Erik Lopez <erik@eriklopez.work>
2017-10-01 15:41:13 -0500

    Updates server for express 4.0

--- server.js
+++ server.js
@@ -18,9 +18,15 @@ let configDB = require('./config/database');
  */
 mongoose.connect(configDB.url); // connect to database
 
-// require('./config/passport')(passport); // pass passport for configuration
-
 // setup express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookes (needed for auth)
+app.use(bodyParser.json()); // get info from html forms
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // setup ejs for templating
+
+// required for passport
 app.use(session({ secret: 'ilovescothyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
